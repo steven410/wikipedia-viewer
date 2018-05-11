27021c3ea76408143360cbc00e2d567f9723c433
steven410 <stevenwh2012@gmail.com>
2018-05-10 22:50:46 -0500

    minor updates

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var searchTerm = function(){
-  var searchValue = document.getElementById("search").value;
-  console.log(searchValue);
-};
-document.getElementById("submit").addEventListener("click", searchTerm);
+
+document.getElementById("submit").addEventListener("click", ajax);
+
+function ajax() {
+  console.log("function ajax was run");
+  var searchTerm = document.getElementById("search").value;
+  console.log(searchTerm);
+  $.ajax({ 
+   type: "GET",
+   url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchTerm + "&prop=info&inprop=url&utf8=&format=json",
+   data: queryData,
+   dataType: "json",
+   headers: { 'Api-User-Agent': 'Example/1.0' },
+   success: function( jsondata ){
+        alert(jsondata.result);
+       }
+  });
+ };
+
