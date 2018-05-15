2f0dcbb16ef28346eeed5c4ac7ad0a5c620308d9
steven410 <stevenwh2012@gmail.com>
2018-05-14 23:06:50 -0500

    added $.getJSON function, JSON is now retreived succesfully

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-
 document.getElementById("submit").addEventListener("click", ajax);
 
-function ajax() {
-  console.log("function ajax was run");
+function ajax(){
+  console.log("function ajax has run");
   var searchTerm = document.getElementById("search").value;
   console.log(searchTerm);
-  $.ajax({ 
-   type: "GET",
-   url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchTerm + "&prop=info&inprop=url&utf8=&format=json",
-   data: queryData,
-   dataType: "json",
-   headers: { 'Api-User-Agent': 'Example/1.0' },
-   success: function( jsondata ){
-        alert(jsondata.result);
-       }
-  });
- };
+        $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&callback=?&suggest=true&search=' + searchTerm, function(json){
+      console.log(json);
+    });
+}
 
+// function ajax() {
+//   console.log("function ajax was run");
+//   var searchTerm = document.getElementById("search").value;
+//   console.log(searchTerm);
+//   $.ajax({ 
+//    url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchTerm + "&prop=info&inprop=url&utf8=&format=json",
+//    data: queryData,
+//    dataType: "json",
+//    success: function( jsondata ){
+//         alert(jsondata.result);
+//        }
+//   });
+//  };
