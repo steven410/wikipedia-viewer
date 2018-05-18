84c48a3d11dfba855f7f792e418018b76a0b4e33
steven410 <stevenwh2012@gmail.com>
2018-05-17 21:45:53 -0500

    minor formatting

--- index.js
+++ index.js
@@ -6,19 +6,7 @@ function ajax(){
   console.log(searchTerm);
         $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&callback=?&suggest=true&search=' + searchTerm, function(json){
       console.log(json);
+
+      
     });
 }
-
-// function ajax() {
-//   console.log("function ajax was run");
-//   var searchTerm = document.getElementById("search").value;
-//   console.log(searchTerm);
-//   $.ajax({ 
-//    url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + searchTerm + "&prop=info&inprop=url&utf8=&format=json",
-//    data: queryData,
-//    dataType: "json",
-//    success: function( jsondata ){
-//         alert(jsondata.result);
-//        }
-//   });
-//  };
