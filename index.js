1e83ba7af33aed88c2a90169e50779c344dc4703
steven410 <stevenwh2012@gmail.com>
2018-05-21 21:45:02 -0500

    working API request. Formatted JSON

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+
 document.getElementById("submit").addEventListener("click", ajax);
+// document.getElementById("search").addEventListener("keydown", function(event){
+//   if (event.keyCode === 13) {
+//     document.getElementById("submit").click();
+//   }
+// });
 
 function ajax(){
-  console.log("function ajax has run");
   var searchTerm = document.getElementById("search").value;
-  console.log(searchTerm);
-        $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&callback=?&suggest=true&search=' + searchTerm, function(json){
-      console.log(json);
-
-      
-    });
-}
+    $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&callback=?&suggest=true&search=' + searchTerm, function(json){
+      for (i=0; i<=json[0].length; i++){
+        $("#results").append(
+            "<h2>" + (json[1][i] + "</h2>" +
+            "<p>"+ json[2][i] + "</p>")
+            );
+      };
+      }); //end function(json)
+    };
